b25bb2e9ed194238bee4484522f1a2a1c1aec049
DavideCriscuolo <davide.alcatel@gmail.com>
2025-06-26 11:00:04 +0200

    Aggiungi file di configurazione iniziale per il progetto Node.js con .gitignore, package.json, package-lock.json, index.html e main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+/*Node package MANAGER   NPM
+
+ci da la possibiltà di importare librerie e plug in vari invece di usare le cdn 
+
+ogni volta che vogliamo creare un progetto con npm andiamoa scrivere nel terminale
+
+-           npm init  
+
+va  a creare un file  package.json  contente  le info del progetto
+
+una volta che scegliamo il pacchetto da installare andiamo su suto di npm e copiamo la riga di comando da incollare nel codice  
+dopo l istallazione ci crea una cartella con scritto node_modules che aggiugngiamo al file gitignore per non pusharla nella repo
+
+
+
+
+
+
+
+*/
