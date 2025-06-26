07e16d98f39055956170638a12ce4c6f029836cd
DavideCriscuolo <davide.alcatel@gmail.com>
2025-06-26 11:51:43 +0200

    Aggiungi script di avvio nel package.json e crea server.js con messaggio di log

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+console.log("Ciao Node");
+
 /*Node package MANAGER   NPM
 
 ci da la possibiltà di importare librerie e plug in vari invece di usare le cdn 
@@ -10,6 +12,31 @@ va  a creare un file  package.json  contente  le info del progetto
 
 una volta che scegliamo il pacchetto da installare andiamo su suto di npm e copiamo la riga di comando da incollare nel codice  
 dopo l istallazione ci crea una cartella con scritto node_modules che aggiugngiamo al file gitignore per non pusharla nella repo
+
+
+
+
+
+
+
+*/
+
+/* Come chiedere a node di eseguire i comandi 
+
+NEL  FILE   package.json 
+
+
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "node server.js"
+  },
+
+in questa sezione se non abbiamo l alias start lo andiamo a creare 
+
+poi dopo nel terminale andiamo a digitare  nmp run start
+
+
+
 
 
 
